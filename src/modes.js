9437a79745fbaeb43cec027f8aca6a8701b428ab
PedroASA <m.amaral.hhs@gmail.com>
2021-03-09 13:25:55 -0300

    Create a record of all modes and initial codes

---
+++ src/modes.js
@@ -0,0 +1,8 @@
+const modes = {
+    'javascript' : "// Type your code here!",
+    'css' : '/* Style here! */',
+    'xml' : '<!-- Leave your marks here! -->',
+    'clike' : "// Type your code here!"
+};
+
+export default modes;
