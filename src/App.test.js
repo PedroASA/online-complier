946d1a593cdde9082d06d7d867af955c87553588
PedroASA <m.amaral.hhs@gmail.com>
2021-03-09 13:24:33 -0300

    Add test to App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', 
+document.createRange = () => {
+    const range = new Range();
+
+    range.getBoundingClientRect = jest.fn();
+
+    range.getClientRects = jest.fn(() => ({
+        item: () => null,
+        length: 0,
+    }));
+
+    return range;
+};
+
+const homeLinkClick = (name) => 
+    test(name, 
+        () => {
+            render(<App />);
+            const link = document.querySelector('#home-link');
+            act(() => {
+                link.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+            });
+            const btn = screen.getByText(/Start Coding/i);
+            expect(btn).toBeInTheDocument();
+        }
+    );
+
+test('renders Start Coding Button (Home Page)', 
 () => {
     render(<App />);
-    const linkElement = screen.getByText(/Start Coding/i);
-    expect(linkElement).toBeInTheDocument();
+    const btn = screen.getByText(/Start Coding/i);
+    expect(btn).toBeInTheDocument();
 });
+
+homeLinkClick('Clicking "Home" link does nothing at first');
+
+
+test('Changes Page on Button Click', 
+() => {
+    render(<App />);
+    const btn = document.querySelector('#code-btn');
+    act(() => {
+        btn.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+    expect(btn).not.toBeInTheDocument(); 
+    expect(document.querySelector('#home-link')).toBeInTheDocument();
+    expect(document.querySelector('#Editor')).toBeInTheDocument();
+    expect(screen.getByText(/Language/i)).toBeInTheDocument();
+});
+
+
+homeLinkClick('Changes Page by Clicking on Home Link');
