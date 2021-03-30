f0539d1b3bb463f934cb93ac9353bee6f2573fc5
PedroASA <m.amaral.hhs@gmail.com>
2021-03-30 14:32:31 -0300

    add package-lock.json

--- api/src/server.js
+++ api/src/server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = 9000
 
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
@@ -11,14 +18,14 @@ modes = require('./modes.js');
 app.post('/code', (req, res) => {
 
   if(!req.body) 
-    res.send(JSON.stringify({
+    res.status(400).json({
       error : "No data was sent!"
-    }));
+    });
 
   const [out, err] = sh.run_code(req.body);
   
   res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
+  res.status(400).json({
     language:req.body.language,
     env:{
         os:"ubuntu 18.04",
@@ -26,12 +33,13 @@ app.post('/code', (req, res) => {
     },
     stdOut: out,
     stdErr: err
-  }))
+  });
 })
 
 app.get('/code', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(modes));
+    console.log('Passed');
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(modes));
 })
 
 app.listen(port, () => {
