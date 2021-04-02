4c5cc0cd8af3da0eb349e7c47acb9ca65d0370df
PedroASA <m.amaral.hhs@gmail.com>
2021-04-02 19:03:27 -0300

    Add enviroment variable for api url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,10 +1,12 @@
+// Set up the proxy between the react server and the api (API_URL).
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
     '/code',
     createProxyMiddleware({
-      target: `http://localhost:9000`,
+      target: process.env.REACT_APP_API_URL,
       changeOrigin: true,
     })
   );
