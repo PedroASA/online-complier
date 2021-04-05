118e2c154e8c9296ebb361cbf5e100c6b57c8136
PedroASA <m.amaral.hhs@gmail.com>
2021-04-05 16:11:36 -0300

    Change json initial message

--- api/src/modes.js
+++ api/src/modes.js
@@ -9,7 +9,7 @@ const modes = {
     'javascript' : ["// Type your code here!", 'js'],
     'cpp'   : ["// Type your code here!", "cpp"],
     'haskell' : ['-- Type your code here!', 'hs'],
-    'json'    : ['{\n\t"body" : {\n\n\t}\n}'],
+    'json'    : ['{}'],
     'html'    : ['<!-- Leave your marks here! -->'],
     'css'     : ['/* Style freely */'],
     'markdown': ['### Title']
